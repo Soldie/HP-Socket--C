587689c48751af370e25f30f7fa1fdfb1e6fcd5b
liangdc <liangdongcheng@lechebang.com>
2016-04-09 14:00:14 +0800

    20160409

--- HP-Socket/Demo/Other Languages Demos/C#/HPSocket4CS/HPSocketCS/Properties/AssemblyInfo.cs
+++ HP-Socket/Demo/Other Languages Demos/C#/HPSocket4CS/HPSocketCS/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("JessMA Open Source")]
 [assembly: AssemblyProduct("HPSocketCS")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // 可以指定所有这些值，也可以使用“内部版本号”和“修订号”的默认值，
 // 方法是按如下所示使用“*”:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("3.2.3")]
-[assembly: AssemblyFileVersion("3.2.3")]
+[assembly: AssemblyVersion("3.4.2")]
+[assembly: AssemblyFileVersion("3.4.2")]
